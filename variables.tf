1718f5036e70367b436899bb7281101721856880
Vinicius Kirst <vinicius.kirst@gmail.com>
2022-07-09 04:02:51 -0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -4,7 +4,9 @@ variable "timezone" {
   default = "America/Sao_Paulo"
 }
 variable "hostname" {}
-variable "host_ip" {}
+variable "host_ip" {
+    default = "127.0.0.1"
+}
 variable "volume_path" {
   default = "/opt/nomad/data/pihole"
 }
